6e7165a970b0b4c6c3bfda7d09ecfcb9307f3a01
ChristoferBC <brenescalvoc@gmail.com>
2022-06-08 14:30:31 -0600

    Holi

--- src/Main/App.java
+++ src/Main/App.java
@@ -3,12 +3,16 @@ package Main;
 public class App {
 
 	public static void main(String[] args) {
-		
-		
-	System.out.println("Hola soy Bryan");
-	
-	System.out.println("Hola, de nuevo soy Bryan");
-	
-	System.out.println("Hola Christofer");
+
+
+		System.out.println("Hola soy Bryan");
+
+		System.out.println("Hola, de nuevo soy Bryan");
+
+		System.out.println("Hola Christofer");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 	}
 }
